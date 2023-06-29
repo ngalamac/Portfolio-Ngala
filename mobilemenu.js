3e0255dd3cc249b98857afe125910b261231f55e
NGALA MAC <macngala4@gmail.com>
2023-06-29 06:58:07 +0100

    Add eslint linters and update workflow

---
+++ mobilemenu.js
@@ -0,0 +1,47 @@
+const menu = {
+  navigation: document.querySelector('.nav-links'),
+  mobileButton: document.querySelector('.mobile-menu'),
+  closeButton: document.querySelector('.close-menu'),
+  linkItems: document.querySelectorAll('.poplink'),
+  isMenuOpen: false,
+
+  toggleMenu() {
+    if (this.isMenuOpen) {
+      this.closeMobileMenu();
+    } else {
+      this.openMobileMenu();
+    }
+  },
+
+  openMobileMenu() {
+    if (!this.isMenuOpen) {
+      this.navigation.classList.add('show');
+      this.mobileButton.classList.add('hide');
+      this.closeButton.classList.add('show');
+      document.body.style.overflow = 'hidden';
+      this.isMenuOpen = true;
+    }
+  },
+
+  closeMobileMenu() {
+    if (this.isMenuOpen) {
+      this.navigation.classList.remove('show');
+      this.mobileButton.classList.remove('hide');
+      this.closeButton.classList.remove('show');
+      document.body.style.overflow = 'auto';
+      this.isMenuOpen = false;
+    }
+  },
+
+  initialize() {
+    this.mobileButton.addEventListener('click', this.toggleMenu.bind(this));
+
+    this.linkItems.forEach((item) => {
+      item.addEventListener('click', this.closeMobileMenu.bind(this));
+    });
+
+    this.closeButton.addEventListener('click', this.closeMobileMenu.bind(this));
+  },
+};
+
+menu.initialize();
