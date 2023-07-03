7da315e630196cf6f7d819a1ce4ed75f028f80bb
NGALA MAC <macngala4@gmail.com>
2023-07-03 05:39:42 +0100

    Update repo

--- application.js
+++ application.js
@@ -1,3 +1,4 @@
+// Menu object
 const menu = {
   navigation: document.getElementById('nav-linksid'),
   mobileButton: document.getElementById('mobile-menuid'),
@@ -15,20 +16,20 @@ const menu = {
 
   openMobileMenu() {
     if (!this.isMenuOpen) {
-      this.navigation.className += ' active';
-      this.mobileButton.className += ' inactive';
-      this.closeButton.className += ' active';
+      this.navigation.classList.add('active');
+      this.mobileButton.classList.add('inactive');
+      this.closeButton.classList.add('active');
       document.body.style.overflow = 'visible';
       this.isMenuOpen = true;
-      this.closeMobileMenu.className += ' inactive';
+      this.closeButton.classList.add('inactive');
     }
   },
 
   closeMobileMenu() {
     if (this.isMenuOpen) {
-      this.navigation.className = this.navigation.className.replace(' active', '');
-      this.mobileButton.className = this.mobileButton.className.replace(' inactive', '');
-      this.closeButton.className = this.closeButton.className.replace(' active', '');
+      this.navigation.classList.remove('active');
+      this.mobileButton.classList.remove('inactive');
+      this.closeButton.classList.remove('active');
       document.body.style.overflow = 'visible';
       this.isMenuOpen = false;
     }
@@ -46,3 +47,160 @@ const menu = {
 };
 
 menu.initialize();
+
+//popup window
+
+const projects = [
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'This is the description of Project 1.',
+    image: '',
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    liveLink: 'https://example.com/project1-live',
+    sourceLink: 'https://github.com/example/project1',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'This is the description of Project 1.',
+    image: '',
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    liveLink: 'https://example.com/project1-live',
+    sourceLink: 'https://github.com/example/project1',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'This is the description of Project 1.',
+    image: '',
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    liveLink: 'https://example.com/project1-live',
+    sourceLink: 'https://github.com/example/project1',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'This is the description of Project 1.',
+    image: '',
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    liveLink: 'https://example.com/project1-live',
+    sourceLink: 'https://github.com/example/project1',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'This is the description of Project 1.',
+    image: '',
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    liveLink: 'https://example.com/project1-live',
+    sourceLink: 'https://github.com/example/project1',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: 'This is the description of Project 1.',
+    image: '',
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    liveLink: 'https://example.com/project1-live',
+    sourceLink: 'https://github.com/example/project1',
+  },
+  // Add more projects following the same structure
+];
+
+const projectsSection = document.getElementById('portfolio');
+const cardsContainer = document.createElement('div');
+cardsContainer.classList.add('cards');
+
+projects.forEach((project, index) => {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const cardImage = document.createElement('div');
+  cardImage.classList.add('cardimage');
+  const image = document.createElement('img');
+  image.src = project.image;
+  cardImage.appendChild(image);
+  card.appendChild(cardImage);
+
+  const more = document.createElement('div');
+  more.classList.add('more');
+
+  const cardTitle = document.createElement('div');
+  cardTitle.classList.add('cardtitle');
+  const title = document.createElement('h3');
+  title.textContent = project.name;
+  cardTitle.appendChild(title);
+  more.appendChild(cardTitle);
+
+  const technologies = document.createElement('div');
+  technologies.classList.add('technologies');
+  const technologiesList = document.createElement('ul');
+  project.technologies.forEach((technology) => {
+    const technologyItem = document.createElement('li');
+    technologyItem.textContent = technology;
+    technologiesList.appendChild(technologyItem);
+  });
+  technologies.appendChild(technologiesList);
+  more.appendChild(technologies);
+
+  const buttonContainer = document.createElement('div');
+  buttonContainer.classList.add('button');
+  const button = document.createElement('button');
+  button.textContent = 'See project';
+  button.addEventListener('click', () => {
+    showProjectDetails(index);
+  });
+  buttonContainer.appendChild(button);
+  more.appendChild(buttonContainer);
+
+  card.appendChild(more);
+  cardsContainer.appendChild(card);
+});
+
+projectsSection.appendChild(cardsContainer);
+
+function showProjectDetails(index) {
+  const project = projects[index];
+
+  // Create the popup window elements
+  const popup = document.createElement('div');
+  popup.classList.add('popup');
+
+  const closeButton = document.createElement('button');
+  closeButton.id = 'closeButton';
+  closeButton.textContent = 'Close';
+  closeButton.addEventListener('click', () => {
+    document.body.removeChild(popup);
+  });
+
+  const details = document.createElement('div');
+  details.classList.add('project-details');
+
+  const title = document.createElement('h2');
+  title.textContent = project.name;
+  details.appendChild(title);
+
+  const description = document.createElement('p');
+  description.textContent = project.description;
+  details.appendChild(description);
+
+  const technologies = document.createElement('div');
+  technologies.classList.add('technologies');
+  const technologiesList = document.createElement('ul');
+  project.technologies.forEach((technology) => {
+    const technologyItem = document.createElement('li');
+    technologyItem.textContent = technology;
+    technologiesList.appendChild(technologyItem);
+  });
+  technologies.appendChild(technologiesList);
+  details.appendChild(technologies);
+
+  const liveLink = document.createElement('a');
+  liveLink.href = project.liveLink;
+  liveLink.textContent = 'Live Version';
+  details.appendChild(liveLink);
+
+  const sourceLink = document.createElement('a');
+  sourceLink.href = project.sourceLink;
+  sourceLink.textContent = 'Source Code';
+  details.appendChild(sourceLink);
+
+  popup.appendChild(closeButton);
+  popup.appendChild(details);
+  document.body.appendChild(popup);
+}
